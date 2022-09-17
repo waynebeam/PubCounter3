6cbd4e051d28bebae3f966087d6090b898dbc99e
waynebeam <waynejcbeam@me.com>
2022-09-17 11:22:57 -0400

    Home button working. Full navigation in place now.

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ class NavigationScreenManager(ScreenManager):
         self.screen_stack = []
         self.publishers = pubs
         self.all_tags = self.find_all_tags()
+        self.main_menu_screen = self.current_screen
 
     def find_all_tags(self) -> []:
         all_tags = []
@@ -86,11 +87,16 @@ class NavigationScreenManager(ScreenManager):
                         matching_pubs.append(pub.title())
         if matching_pubs:
             title_str = "who match " + ", ".join(tags)
-            self.show_name_list_screen(matching_pubs, title_str)
+            return True, matching_pubs, title_str
         else:
-            pass  # just pass for now. I think This should be impossible to reach since we get here off of the
-            # select screen. So it should always be a valid list, right? But when we implement terminal we might
-            # need a check like this
+            return False, [], "No matches found"
+
+    def return_to_main_menu_screen(self):
+        self.transition.direction = "right"
+        self.screen_stack.clear()
+        self.add_widget(self.main_menu_screen)
+        self.remove_widget(self.current_screen)
+        self.current = "main_menu_screen"
 
     def change_screens(self, next_screen: Screen):
         self.screen_stack.append(self.current_screen)
@@ -111,6 +117,14 @@ class NavigationScreenManager(ScreenManager):
 class ListScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.layout = BoxLayout(orientation="vertical")
+        navigation_bar = NavigationBar()
+        navigation_bar.back_btn.bind(on_release=self.back_btn_bind)
+        navigation_bar.home_btn.bind(on_release=self.bind_home_btn)
+        self.layout.add_widget(navigation_bar)
+
+    def bind_home_btn(self, btn):
+        self.manager.return_to_main_menu_screen()
 
     def back_btn_bind(self, btn):
         self.manager.go_back()
@@ -122,8 +136,6 @@ class ListScreen(Screen):
     def bind_scroll_bottom_button(self, btn):
         self.names_scroll_section.scroll_y = 0
 
-    def bind_search_btn(self, btn):
-        self.manager.create_matching_pubs_screen(self.tags_to_search)
 
     def bind_name_btn(self, btn):
         self.manager.show_single_name_screen(btn.text.lower())
@@ -133,14 +145,11 @@ class SingleNameScreen(ListScreen):
     def __init__(self, pub: Publisher, **kwargs):
         super().__init__(**kwargs)
         self.name = "name_screen"
-        layout = BoxLayout(orientation="vertical")
-        back_btn = BackButton()
-        back_btn.bind(on_release=self.back_btn_bind)
-        layout.add_widget(back_btn)
+
         name_label = Label(text=pub["name"].title(), size_hint=(1, .3))
-        layout.add_widget(name_label)
+        self.layout.add_widget(name_label)
         tags_scroll_section = ScrollView()
-        layout.add_widget(tags_scroll_section)
+        self.layout.add_widget(tags_scroll_section)
         tags_grid = GridLayout(cols=1, size_hint_y=None, spacing=2)
         tags_grid.bind(minimum_height=tags_grid.setter("height"))
         for tag in pub["tags"]:
@@ -148,24 +157,24 @@ class SingleNameScreen(ListScreen):
             tags_grid.add_widget(btn)
             btn.bind(on_release=self.bind_tag_btn)
         tags_scroll_section.add_widget(tags_grid)
-        self.add_widget(layout)
+        self.add_widget(self.layout)
 
 
-class BackButton(Button):
+class NavigationBar(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.text = "back"
-        self.size_hint = (1, .2)
+        self.size_hint = (1, .1)
+        self.back_btn = Button(text="back", size_hint=(.8, 1))
+        self.home_btn = Button(text="home", size_hint=(.2, 1))
+        self.add_widget(self.home_btn)
+        self.add_widget(self.back_btn)
 
 
 class NameListScreen(ListScreen):
     def __init__(self, names: [], list_type: str = "in all", **kwargs):
         super().__init__(**kwargs)
         self.name = "namelist"
-        layout = BoxLayout(orientation="vertical")
-        back_btn = BackButton()
-        back_btn.bind(on_release=self.back_btn_bind)
-        layout.add_widget(back_btn)
+
         title_layout = BoxLayout(size_hint=(1, .2))
         title_label = Label(text=f"{len(names)} Publishers {list_type}", size_hint=(.8, 1))
         title_layout.add_widget(title_label)
@@ -175,7 +184,7 @@ class NameListScreen(ListScreen):
             scroll_bottom_btn.bind(on_release=self.bind_scroll_bottom_button)
             title_layout.add_widget(scroll_bottom_btn)
 
-        layout.add_widget(title_layout)
+        self.layout.add_widget(title_layout)
         self.names_scroll_section = ScrollView(bar_width=10, bar_margin=5, effect_cls="ScrollEffect")
         names_grid = GridLayout(cols=2, size_hint_y=None, spacing=2)
         names_grid.bind(minimum_height=names_grid.setter("height"))
@@ -184,8 +193,8 @@ class NameListScreen(ListScreen):
             names_grid.add_widget(btn)
             btn.bind(on_release=self.bind_name_btn)
         self.names_scroll_section.add_widget(names_grid)
-        layout.add_widget(self.names_scroll_section)
-        self.add_widget(layout)
+        self.layout.add_widget(self.names_scroll_section)
+        self.add_widget(self.layout)
 
 
 class AllTagsScreen(ListScreen):
@@ -194,17 +203,14 @@ class AllTagsScreen(ListScreen):
         self.number_of_tags = len(tags)
         self.tags_to_search = []
         self.search_or = False
-        layout = BoxLayout(orientation="vertical")
-        back_btn = BackButton()
-        back_btn.bind(on_release=self.back_btn_bind)
-        layout.add_widget(back_btn)
+
         title_layout = BoxLayout(size_hint=(1, .2))
         self.title_label = Label(text=f"Choose from the {self.number_of_tags} tags", size_hint=(.8, 1))
         or_toggle_btn = Button(text="Matching ALL", size_hint=(.2, 1))
         or_toggle_btn.bind(on_release=self.bind_or_toggle_btn)
         title_layout.add_widget(self.title_label)
         title_layout.add_widget(or_toggle_btn)
-        layout.add_widget(title_layout)
+        self.layout.add_widget(title_layout)
         tags_scroll_section = ScrollView()
         tags_grid = GridLayout(cols=2, size_hint_y=None, spacing=2)
         tags_grid.bind(minimum_height=tags_grid.setter("height"))
@@ -213,11 +219,11 @@ class AllTagsScreen(ListScreen):
             tags_grid.add_widget(btn)
             btn.bind(on_release=self.bind_tag_btn)
         tags_scroll_section.add_widget(tags_grid)
-        layout.add_widget(tags_scroll_section)
+        self.layout.add_widget(tags_scroll_section)
         self.search_btn = Button(text="Search", size_hint=(1, .2), disabled=True)
         self.search_btn.bind(on_release=self.bind_search_btn)
-        layout.add_widget(self.search_btn)
-        self.add_widget(layout)
+        self.layout.add_widget(self.search_btn)
+        self.add_widget(self.layout)
 
     def bind_tag_btn(self, btn):
         if btn.text not in self.tags_to_search:
@@ -268,6 +274,14 @@ class AllTagsScreen(ListScreen):
             self.tags_to_search.remove("and")
             self.tags_to_search.insert(-1, "or")
             self.title_label.text = str.replace(self.title_label.text, "and", "or")
+
+    def bind_search_btn(self, btn):
+        result, title_str, matching_pubs = self.manager.create_matching_pubs_screen(self.tags_to_search)
+        if result:
+            self.manager.show_name_list_screen(title_str,matching_pubs)
+        else:
+            self.title_label.text = "No matches found"
+            self.tags_to_search.clear()
 
 
 if __name__ == '__main__':
